edf2160bc72e7e6fc37c96be653cc20eca6fb9cc
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-08-30 10:01:50 +0500

    constants file

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const imageStore = Object.freeze({
+  ADD: 0,
+  UPDATE: 1,
+  REMOVE_SELECTED: 2
+})
+
+export const imageSelect = Object.freeze({
+  CHECK_CHANGE: 0,
+  CHECK_ALL: 1,
+  UNCHECK_ALL: 2
+})
+
+export const imageStatus = Object.freeze({
+  READY: 0,
+  COMPRESSING: 1,
+  COMPRESSED: 2,
+  FAILED: 3
+})
