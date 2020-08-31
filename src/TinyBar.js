789fd40a68fbb2c856f594ae7b67a5131ec2e703
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-08-31 19:24:21 +0500

    update design

--- src/TinyBar.js
+++ src/TinyBar.js
@@ -1,34 +1,44 @@
 import React from 'react'
 import { formControl } from './styles'
-import { Stack, Input } from './elements'
+import { Input } from './elements'
 import styled from '@emotion/styled'
+
+const Grid = styled.div`
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: auto ${props => props.count && 'max-content'} max-content;
+  margin-bottom: 16px;
+`
 
 const Checkbox = styled.div`
   ${formControl}
 
   align-items: center;
   display: flex;
-  margin-left: 16px;
-  padding: 0 8px;
 `
 
-const TinyBar = ({ apiKey, handleApiKey, byTiny, handleByTiny }) => {
+const Compressions = styled.div`
+  ${formControl}
+`
+
+const TinyBar = ({ compressionCount, apiKey, handleApiKey, tinify, handleTinify }) => {
   return (
-    <Stack>
+    <Grid count={tinify}>
       <Input
         type="text"
         value={apiKey}
         onChange={handleApiKey}
-        disabled={!byTiny}
+        disabled={!tinify}
         placeholder="Enter your API Key here" />
+      {tinify && <Compressions>Compression count: <i>{compressionCount || 'Unkown'}</i></Compressions>}
       <Checkbox>
         <label>TinyPNG</label>
         <input
           type="checkbox"
-          checked={byTiny}
-          onChange={handleByTiny} />
+          checked={tinify}
+          onChange={handleTinify} />
       </Checkbox>
-    </Stack>
+    </Grid>
   )
 }
 
