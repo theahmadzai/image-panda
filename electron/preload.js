f6bcb980f0a7346cfdaf376d2663425072393db8
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-08-31 19:25:19 +0500

    get key from file on load

---
+++ electron/preload.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.readFile(path.resolve(__dirname, '../.key'), (err, data) => {
+  if (!err) {
+    window.TINY_API_KEY = data.toString().trim()
+  }
+})
