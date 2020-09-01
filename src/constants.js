56a69a848e539463de79cafd44c202766d0db56e
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-01 21:27:42 +0500

    delete uploading state

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,17 @@
-exports.imageStore = Object.freeze({
+module.exports.imageStore = Object.freeze({
   ADD: 0,
   UPDATE: 1,
   REMOVE_SELECTED: 2
 })
 
-exports.imageSelect = Object.freeze({
+module.exports.imageSelect = Object.freeze({
   CHECK_CHANGE: 0,
   CHECK_ALL: 1,
   UNCHECK_ALL: 2
 })
 
-exports.imageStatus = Object.freeze({
+module.exports.imageStatus = Object.freeze({
   STARTED: 'STARTED',
-  UPLOADING: 'UPLOADING',
   COMPRESSING: 'COMPRESSING',
   COMPRESSED: 'COMPRESSED',
   FAILED: 'FAILED'
