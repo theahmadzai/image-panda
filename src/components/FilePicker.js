19dd9557036d4562a0d544db65aca9440303eeb5
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-03 09:23:43 +0500

    refactor and move to components

---
+++ src/components/FilePicker.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Input } from '../elements'
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 8px;
+`
+
+const FilePicker = props => {
+  const { label } = props
+  const inputProps = { ...props }
+  delete inputProps.label
+
+  return (
+    <div>
+      <Label>{label}</Label>
+      <Input
+        {...inputProps}
+        type="text"
+        readOnly />
+    </div>
+  )
+}
+
+export default React.memo(FilePicker)
