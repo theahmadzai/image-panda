1c2ab10c6ca55dc7ae4dac9500ff48137b814f63
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-03 09:24:13 +0500

    refactor

---
+++ electron/dialogs.js
@@ -0,0 +1,48 @@
+const { dialog } = require('electron')
+
+const inputPathOptions = {
+  filters: [
+    {
+      name: 'Images',
+      extensions: ['jpg', 'png', 'gpeg', 'gif', 'svg']
+    }
+  ],
+  properties: ['multiSelections']
+}
+
+const outputPathOptions = {
+  properties: ['openDirectory']
+}
+
+const openDialog = options => {
+  return dialog.showOpenDialog(null, options)
+    .then(({ canceled, filePaths }) => {
+      return (!canceled && filePaths.length) && filePaths
+    })
+}
+
+const getImagesFromUser = () => {
+  return openDialog(inputPathOptions)
+    .catch(() => {
+      dialog.showErrorBox(
+        'Error opening files',
+        'Failed to open image file.'
+      )
+    })
+}
+
+const getDirectoryFromUser = () => {
+  return openDialog(outputPathOptions)
+    .then(filePaths => filePaths && filePaths[0])
+    .catch(() => {
+      dialog.showErrorBox(
+        'Error selecting directory',
+        'Failed to select a directory.'
+      )
+    })
+}
+
+module.exports = {
+  getImagesFromUser,
+  getDirectoryFromUser
+}
