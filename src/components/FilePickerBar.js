0ae3f93f3820db48f53edff1e965f6b541e35713
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-09 18:00:49 +0500

    refactor from App to FilePickerBar

---
+++ src/components/FilePickerBar.js
@@ -0,0 +1,69 @@
+import React, { useContext, useEffect } from 'react'
+import styled from '@emotion/styled'
+import { baseGrid } from '../styles'
+import { AppContext } from '../AppContext'
+import { ImagesContext } from '../ImagesContext'
+import { imageActionType } from '../constants'
+import FilePicker from './FilePicker'
+import { pathsToImageObjects } from '../utils'
+const { remote } = window.require('electron')
+const { getImagesFromUser, getDirectoryFromUser } = remote.require('./electron/dialogs.js')
+
+const Grid = styled.div`
+  ${baseGrid}
+
+  grid-template-columns: auto auto;
+`
+
+const FilePickerBar = () => {
+  const { app, setApp } = useContext(AppContext)
+  const { images, dispatchImages } = useContext(ImagesContext)
+
+  const { inputPath, outputPath, compressing } = app
+
+  useEffect(() => {
+    setApp(state => {
+      const inputPath = images.size === 0 ? ''
+        : images.size === 1 ? images.keys().next().value
+          : `${images.size} images selected`
+
+      return { ...state, inputPath }
+    })
+  }, [images, setApp])
+
+  const handleInputPicker = async () => {
+    const filePaths = await getImagesFromUser()
+
+    filePaths && dispatchImages({
+      type: imageActionType.SET,
+      payload: pathsToImageObjects(filePaths)
+    })
+  }
+
+  const handleOutputPicker = async () => {
+    const directory = await getDirectoryFromUser()
+
+    directory && setApp(state => ({
+      outputPath: directory
+    }))
+  }
+
+  return (
+    <Grid>
+      <FilePicker
+        label='Input Files'
+        placeholder='No images selected'
+        disabled={compressing}
+        value={inputPath}
+        onClick={handleInputPicker} />
+      <FilePicker
+        label='Output Directory'
+        placeholder='Select folder...'
+        disabled={compressing}
+        value={outputPath}
+        onClick={handleOutputPicker} />
+    </Grid>
+  )
+}
+
+export default FilePickerBar
