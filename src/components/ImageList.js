7a68a8cf66e25604b4c17e182cdcc1a9aa75fd22
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-09 18:01:42 +0500

    refactor ImageList

---
+++ src/components/ImageList.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useCallback } from 'react'
+import styled from '@emotion/styled'
+import { imageActionType, imageStatus } from '../constants'
+import { useImages } from '../ImagesContext'
+import ImageListHeader from './ImageListHeader'
+import ImageListItem from './ImageListItem'
+const { ipcRenderer } = window.require('electron')
+
+const List = styled.div`
+  background: #fff;
+  color: #000;
+  height: 350px;
+  margin-bottom: 16px;
+  overflow: scroll;
+  position: relative;
+  width: 100%;
+
+  div {
+    align-items: center;
+    display: grid;
+    font-size: 14px;
+    font-weight: 400;
+    grid-template-columns: auto auto auto auto auto;
+
+    div {
+      padding: 4px;
+    }
+  }
+
+  & > :first-of-type {
+    background: #f0f0f0;
+    color: #000;
+    height: 35px;
+    left: 0;
+    position: sticky;
+    top: 0;
+  }
+`
+
+const ImageList = () => {
+  const { images, dispatchImages } = useImages()
+
+  useEffect(() => {
+    Object.values(imageStatus).forEach(status => {
+      ipcRenderer.on(status, (e, { filePath, ...meta }) => {
+        dispatchImages(({
+          type: imageActionType.CHANGE_STATUS,
+          payload: { status, filePath, meta }
+        }))
+      })
+    })
+  }, [dispatchImages])
+
+  const handleCheckChange = useCallback(key => {
+    dispatchImages({
+      type: imageActionType.CHECK_CHANGE,
+      payload: key
+    })
+  }, [dispatchImages])
+
+  return (
+    <List>
+      <ImageListHeader/>
+      {Array.from(images, ([filePath, image]) => (
+        <ImageListItem
+          key={filePath}
+          filePath={filePath}
+          onCheckChange={handleCheckChange}
+          image={image} />
+      ))}
+    </List>
+  )
+}
+
+export default ImageList
---
+++ src/components/ImageListItem.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from 'react'
+import filesize from 'filesize'
+import { imageStatus } from '../constants'
+
+const ImageListItem = ({ filePath, image, onCheckChange }) => {
+  const { selected, status, meta } = image
+
+  console.log('called')
+  let row = null
+
+  switch (status) {
+    case imageStatus.COMPRESSING:
+      row = (
+        <Fragment>
+          <div>{filesize(meta.originalSize)}</div>
+          <div>COMPRESSING</div>
+        </Fragment>
+      )
+      break
+
+    case imageStatus.COMPRESSED:
+      meta.savedSize = meta.originalSize - meta.currentSize
+      meta.savedPercentage = Math.ceil(((meta.currentSize / meta.originalSize) * 100) - 100)
+      row = (
+        <Fragment>
+          <div>{filesize(meta.originalSize)}</div>
+          <div>{filesize(meta.currentSize)}</div>
+          <div>{filesize(meta.savedSize)} ({meta.savedPercentage}%)</div>
+        </Fragment>
+      )
+      break
+
+    case imageStatus.FAILED:
+      row = (
+        <Fragment>
+          <div>FAILED</div>
+          <div>{meta.error}</div>
+        </Fragment>
+      )
+      break
+
+    default:
+      row = (
+        <div>READY</div>
+      )
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="checkbox"
+          checked={selected}
+          onChange={onCheckChange.bind(null, filePath)} />
+      </div>
+      <div>{filePath.substring(filePath.lastIndexOf('\\') + 1)}</div>
+      {row}
+    </div>
+  )
+}
+
+export default React.memo(ImageListItem)
