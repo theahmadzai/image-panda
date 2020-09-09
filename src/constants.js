1ffe22d71cb044f37650ec3dab917d830242fe7b
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-09 17:56:33 +0500

    update constants

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,19 @@
-module.exports.imageStore = Object.freeze({
-  ADD: 0,
-  UPDATE: 1,
-  REMOVE_SELECTED: 2
-})
-
-module.exports.imageSelect = Object.freeze({
-  CHECK_CHANGE: 0,
-  CHECK_ALL: 1,
-  UNCHECK_ALL: 2
+module.exports.imageActionType = Object.freeze({
+  SET: 0,
+  ADD: 1,
+  REMOVE_SELECTED: 2,
+  CHECK_CHANGE: 3,
+  CHECK_CHANGE_ALL: 4,
+  CHANGE_STATUS: 5
 })
 
 module.exports.imageStatus = Object.freeze({
-  STARTED: 0,
-  COMPRESSING: 1,
-  COMPRESSED: 2,
-  FAILED: 3
+  COMPRESSING: 'COMPRESSING',
+  COMPRESSED: 'COMPRESSED',
+  FAILED: 'FAILED'
 })
+
+module.exports.READY_TO_COMPRESS = 'READY_TO_COMPRESS'
+module.exports.COMPRESSION_START = 'COMPRESSION_START'
+module.exports.COMPRESSION_STATUS = 'COMPRESSION_DONE'
+module.exports.COMPRESSION_COUNT = 'COMPRESSION_COUNT'
