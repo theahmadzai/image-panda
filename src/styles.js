4cd50dc614738ca1ffc474ccb51ad885d2006e5e
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-10 18:41:21 +0500

    global styles export

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,27 @@
 import { css } from '@emotion/core'
 
+export const globalStyles = css`
+  body {
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    color: #282c34;
+    font-family:
+      -apple-system,
+      BlinkMacSystemFont,
+      'Segoe UI',
+      'Roboto',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      'Fira Sans',
+      'Droid Sans',
+      'Helvetica Neue',
+      sans-serif;
+    margin: 0;
+    padding: 16px;
+    padding-bottom: 0;
+  }
+`
 export const resetElement = css`
   border: 0;
   box-sizing: border-box;
