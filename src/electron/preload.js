f461e569f535156d633093948b5333efdc183be5
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-14 03:00:49 +0500

    provide api for renderer

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -1,8 +1,28 @@
 const fs = require('fs')
 const path = require('path')
+const { remote, ipcRenderer, contextBridge, shell } = require('electron')
+const { getImagesFromUser, getDirectoryFromUser } = remote.require('./electron/dialogs.js')
 
-fs.readFile(path.resolve(__dirname, '../../.key'), (err, data) => {
-  if (!err) {
-    window.TINY_API_KEY = data.toString().trim()
+const apiKey = fs.readFileSync(path.resolve(__dirname, '../../.key'))
+  .toString()
+  .trim()
+
+contextBridge.exposeInMainWorld('tinify', {
+  apiKey
+})
+
+contextBridge.exposeInMainWorld('dialog', {
+  getImagesFromUser,
+  getDirectoryFromUser
+})
+
+contextBridge.exposeInMainWorld('ipcRenderer', {
+  on: (event, data) => ipcRenderer.on(event, data),
+  send: (channel, arg) => ipcRenderer.send(channel, arg)
+})
+
+contextBridge.exposeInMainWorld('shell', {
+  openTinyPngApiDocs: () => {
+    shell.openExternal('https://tinypng.com/developers')
   }
 })
