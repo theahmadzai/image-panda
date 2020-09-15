b3f095cf0eb5c1244fac388454ac71c0049e0e3b
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-15 18:51:36 +0500

    add notification functionality

--- src/electron/WindowEventEmitter.js
+++ src/electron/WindowEventEmitter.js
@@ -1,4 +1,7 @@
 const EventEmitter = require('events')
+const { Notification } = require('electron')
+const { ICON_PATH } = require('../constants')
+const filesize = require('filesize')
 
 class WindowEventEmitter extends EventEmitter {
   forwardEventsToWindow (events, win) {
@@ -7,6 +10,18 @@ class WindowEventEmitter extends EventEmitter {
         win.webContents.send(status, msg)
       })
     })
+  }
+
+  notify ({ imagesCompressed, bytesSaved, timeTaken = new Date(0) }) {
+    const time = (timeTaken.getMinutes() > 0) ? `${timeTaken.getMinutes()}m ${timeTaken.getSeconds()}s`
+      : (timeTaken.getSeconds() > 0) ? `${timeTaken.getSeconds()} seconds`
+        : `${timeTaken.getMilliseconds()}ms`
+
+    new Notification({
+      title: `${imagesCompressed} images compressed`,
+      body: `${filesize(bytesSaved)} size saved in ${time}`,
+      icon: ICON_PATH
+    }).show()
   }
 }
 
