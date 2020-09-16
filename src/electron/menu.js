b73210a1cf8494c41b13e2e2e2c6a52e9f8de4ad
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-17 02:10:10 +0500

    update menu

--- src/electron/menu.js
+++ src/electron/menu.js
@@ -1,11 +1,65 @@
+const { shell } = require('electron')
+const isDev = require('electron-is-dev')
+const { APP_NAME } = require('../constants')
+
+const isMac = process.platform === 'darwin'
+
 const menu = [
-  { role: 'reload' },
-  { role: 'toggledevtools' }
+  ...(isMac ? [{
+    label: APP_NAME,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    label: 'Help',
+    submenu: [
+      { label: 'Learn more', click: async () => await shell.openExternal('https://github.com/theahmadzai/image-panda') },
+      { type: 'separator' },
+      { label: 'Report Issue', click: async () => await shell.openExternal('https://github.com/theahmadzai/image-panda/issues') },
+      { label: 'Suggest Feature', click: async () => await shell.openExternal('https://github.com/theahmadzai/image-panda/issues') },
+      { type: 'separator' },
+      { label: 'Check for updates' }
+    ]
+  }
 ]
 
-if (process.platform === 'darwin') {
-  menu.unshift({
-    label: 'ApplicationName'
+if (isDev) {
+  menu.push({
+    label: 'Developer',
+    submenu: [
+      { role: 'reload' },
+      { role: 'toggledevtools' }
+    ]
   })
 }
 
