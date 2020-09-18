0f1fa3f30a2d8803da79e00bf5da6759579281ad
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-19 04:10:15 +0500

    fix build path, add sentry, updater and remove apiKey to file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-require('update-electron-app')({
-  repo: 'theahmadzai/image-panda',
-  updateInterval: '1 hour'
-  // logger: require('electron-log')
-})
 const { BrowserWindow, Menu, app, ipcMain, dialog } = require('electron')
 const path = require('path')
 const fs = require('fs')
 const isDev = require('electron-is-dev')
+const Sentry = require('@sentry/electron')
+const updater = require('./electron/updater')
 const menu = require('./electron/menu')
 const { getImagesFromUser, getDirectoryFromUser } = require('./electron/dialogs.js')
 const TinifyCompressor = require('./electron/TinifyCompressor')
@@ -26,6 +23,8 @@ const eventList = [
   COMPRESSION_STATUS,
   COMPRESSION_COUNT
 ]
+
+Sentry.init({ dsn: 'https://bdbb83d8ca1c4fab9bdb7d92eada13b2@o449876.ingest.sentry.io/5433630' })
 
 let mainWindow
 
@@ -49,11 +48,13 @@ const createWindow = () => {
 
   mainWindow.loadURL(isDev
     ? 'http://127.0.0.1:3000'
-    : `file://${path.join(__dirname, 'build/index.html')}`)
+    : `file://${path.join(__dirname, '../build/index.html')}`)
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
   })
+
+  updater()
 }
 
 app.whenReady().then(createWindow)
@@ -72,13 +73,19 @@ ipcMain.handle(GET_DIRECTORY_FROM_USER, getDirectoryFromUser)
 
 ipcMain.on(COMPRESSION_START, (e, { filePaths, app: { useTinify, apiKey, outputPath } }) => {
   if (filePaths.length <= 0) {
-    dialog.showErrorBox('No files selected', 'You must select files first.')
+    dialog.showErrorBox(
+      'No files to compress',
+      'No uncompressed files preset, Please add files to compress.'
+    )
     return
   }
 
   fs.access(outputPath, fs.constants.F_OK, err => {
     if (err) {
-      dialog.showErrorBox('Invalid output directory', 'Output directory not readable.')
+      dialog.showErrorBox(
+        'Invalid output directory',
+        'Output directory does not exist or is not readable.'
+      )
       return
     }
 
@@ -90,10 +97,6 @@ ipcMain.on(COMPRESSION_START, (e, { filePaths, app: { useTinify, apiKey, outputP
 
 const selectCompressor = (useTinify, apiKey, filePaths = [], dest) => {
   if (useTinify) {
-    fs.writeFile(path.join(__dirname, '../.key'), apiKey, 'utf-8', (err) => {
-      if (err) {} // ignore
-    })
-
     return new TinifyCompressor(apiKey, filePaths, dest)
   }
 
